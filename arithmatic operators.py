cf93a4a62615f47bfbb903ff54e425dd3c1e9bcd
bahgat <b.alaliwi@hotmail.com>
2020-10-31 07:08:05 +0100

    Create arithmatic operators.py

---
+++ arithmatic operators.py
@@ -0,0 +1,21 @@
+#create two variables
+a=10
+b=20
+# addition (+) operator
+print("a+b")
+print(a+b)
+# subtraction (-) operator
+print("a-b")
+print(a-b)
+# multiplication (*) operator
+print("a*b")
+print(a*b)
+# division (/) operator
+print("b/a")
+print(b/a)
+# modulus (%) operator
+print("a%b")
+print(a%b) # prints the remainder of a/b
+# exponent (**) operator
+print("a**b")
+print(a**b) #prints a^b
